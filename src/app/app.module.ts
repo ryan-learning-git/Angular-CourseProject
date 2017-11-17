7b5ff1745b1cb6201192acbbff658a923aa9182c
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-17 11:33:47 +0000

    Completed Lesson 42 - adding all the components we'll need.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import { HeaderComponent } from './PageFurniture/header/header.component';
+import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeBookComponent } from './RecipeBook/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
+import { IngredientComponent } from './Data/ingredient/ingredient.component';
+import { RecipeComponent } from './Data/recipe/recipe.component';
+import { RecipesComponent } from './RecipeBook/recipes/recipes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    IngredientComponent,
+    RecipeComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
