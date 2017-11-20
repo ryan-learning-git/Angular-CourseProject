422df52a10de19e4166414a2cee06e72a73b83a1
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-20 10:31:58 +0000

    Completed Section 2

--- src/app/ShoppingList/shopping-list/shopping-list.component.ts
+++ src/app/ShoppingList/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../Shared/Data/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 50),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
 
